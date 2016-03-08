3ce6cc319b1853f826c1ed6c8a3dd22d3b46c5c4
Bilal Mian <Bilal@Bilals-MacBook-Pro.local>
2016-03-08 18:26:40 -0500

    Pokemon Trivia Game

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,151 +1,160 @@
-	var stopwatch = {
-		number: 31,
-		run: function () {
-			counter = setInterval(stopwatch.increment, 1000);
-		},
-		increment: function() {
-			stopwatch.number--;
-	      	$('#show-number').html('<h2>' + stopwatch.number + '</h2>');
-	      	if (stopwatch.number === 0){
-	        	stopwatch.stop();
-			}
-		},
-		stop: function() {
-			clearInterval(counter);
+var stopwatch = {
+	number: 31,
+	run: function () {
+		counter = setInterval(stopwatch.increment, 1000);
+	},
+	increment: function() {
+		stopwatch.number--;
+      	$('#show-number').html('<h2>' + stopwatch.number + '</h2>');
+      	if (stopwatch.number === 0){
+        	stopwatch.stop();
 		}
-	};
+	},
+	stop: function() {
+		clearInterval(counter);
+	}
+};
 
-	var triviaGame = {
-		quiz: [ {
-				question: "Who's that Pokemon?",
-				picture: "assets/images/pikachu.png",
-				choices: ['Bulbasaur','Pikachu','Chansey','Diglet'],
-				answer: 1,
-			},
-			{	question: "Who's that Pokemon?s",
-				picture: 'assets/images/alakazam.png',
-				choices: ['Mewtwo','Weedle','Metapod','Alakazam'],
-				answer: 3,
-			},
-			{	question: "Who's that Pokemon?",
-				picture: 'assets/images/scyther.png',
-				choices: ['Beedrill','Victreebel','Scyther','Pidgeot'],
-				answer: 2,
-			},
-			{	question: "Who's that Pokemon?",
-				picture: 'assets/images/magmar.png',
-				choices: ['Magmar','Charmeleon','Koffing','Snorlax'],
-				answer: 0,
-			},
-			{	question: "Who's that Pokemon?",
-				picture: 'assets/images/ditto.png',
-				choices: ['Grimer','Ekans','Hitmonlee','Ditto'],
-				answer: 3,
-			},
-			{	question: "Who's that Pokemon?",
-				picture: 'assets/images/zapdos.png',
-				choices: ['Articuno','Moltres','Zapdos','Lugia'],
-				answer: 2,
-			},
-			{	question: "Who's that Pokemon?",
-				picture: 'assets/images/haunter.png',
-				choices: ['Vulpix','Zubat','Haunter','Venonat'],
-				answer: 2,
-			},
-			{	question: "Who's that Pokemon?",
-				picture: 'assets/images/growlithe.png',
-				choices: ['Growlithe','Gyarados','Pinsir','Meowth'],
-				answer: 0,
-			},
-			{	question: "Who's that Pokemon?",
-				picture: 'assets/images/vileplume.png',
-				choices: ['Tentacool','Muk','Shellder','Vileplume'],
-				answer: 3,
-			},
-			{	question: "Who's that Pokemon?",
-				picture: 'assets/images/exeggutor.png',
-				choices: ['Exeguutor','Mr. Mime','Venusaur','Clefable'],
-				answer: 0,
-			},
-			{	question: "Who's that Pokemon?",
-				picture: 'assets/images/mew.png',
-				choices: ['Drowzee','Mew','Kingler','Dratini'],
-				answer: 1,
-			}],
+var quiz = [ {
+		question: "Who's that Pokemon?",
+		picture: "assets/images/pikachu.png",
+		choices: ['Bulbasaur','Pikachu','Chansey','Diglet'],
+		correct: 1,
+	},
+	{	question: "Who's that Pokemon?",
+		picture: 'assets/images/alakazam.png',
+		choices: ['Mewtwo','Weedle','Metapod','Alakazam'],
+		correct: 3,
+	},
+	{	question: "Who's that Pokemon?",
+		picture: 'assets/images/scyther.png',
+		choices: ['Beedrill','Victreebel','Scyther','Pidgeot'],
+		correct: 2,
+	},
+	{	question: "Who's that Pokemon?",
+		picture: 'assets/images/magmar.png',
+		choices: ['Magmar','Charmeleon','Koffing','Snorlax'],
 		correct: 0,
-		incorrect: 0,
-		counter:0
+	},
+	{	question: "Who's that Pokemon?",
+		picture: 'assets/images/ditto.png',
+		choices: ['Grimer','Ekans','Hitmonlee','Ditto'],
+		correct: 3,
+	},
+	{	question: "Who's that Pokemon?",
+		picture: 'assets/images/zapdos.png',
+		choices: ['Articuno','Moltres','Zapdos','Lugia'],
+		correct: 2,
+	},
+	{	question: "Who's that Pokemon?",
+		picture: 'assets/images/haunter.png',
+		choices: ['Vulpix','Zubat','Haunter','Venonat'],
+		correct: 2,
+	},
+	{	question: "Who's that Pokemon?",
+		picture: 'assets/images/growlithe.png',
+		choices: ['Growlithe','Gyarados','Pinsir','Meowth'],
+		correct: 0,
+	},
+	{	question: "Who's that Pokemon?",
+		picture: 'assets/images/vileplume.png',
+		choices: ['Tentacool','Muk','Shellder','Vileplume'],
+		correct: 3,
+	},
+	{	question: "Who's that Pokemon?",
+		picture: 'assets/images/exeggutor.png',
+		choices: ['Exeguutor','Mr. Mime','Venusaur','Clefable'],
+		correct: 0,
+	},
+	{	question: "Who's that Pokemon?",
+		picture: 'assets/images/mew.png',
+		choices: ['Drowzee','Mew','Kingler','Dratini'],
+		correct: 1,
+}];
 
-		setup: function() {
-			$('#card').hide();
+var numQuestions = quiz.length;
+var numCorrect = 0;
+var counter = 0;
 
-		},
+var pokePic = $('<img>');
 
-		start: function () {
-			this.setup();
-			
-			$('#begin').on('click', function(){
-				$('#begin').hide();
-				$('#card').show();
-				triviaGame.reveal();
-				
-			});
-					
-		},
 
-		reveal: function () {
 
-			var pokeImage = $('<img>');
+$('input[name="choice"]').hide;
 
-			for (var i = 0; i < this.quiz.length; i++) {
-				this.counter = i;
-				document.querySelector('#question').innerHTML = this.quiz[i].question;
-				pokeImage.attr("src", this.quiz[i].picture);
-				$('#pokes').append(pokeImage);
+// display question function
+function nextQuest(){
 
-				document.querySelector('#answer0').innerHTML = this.quiz[i].choices[0];
-				document.querySelector('#answer1').innerHTML = this.quiz[i].choices[1];
-				document.querySelector('#answer2').innerHTML = this.quiz[i].choices[2];
-				document.querySelector('#answer3').innerHTML = this.quiz[i].choices[3];
+	$('#questions').text(quiz[counter].question);
+	pokePic.attr('src', quiz[counter].picture)
+	$('#pokes').append(pokePic)
+	$('#answer0').text(quiz[counter].choices[0]);
+	$('#answer1').text(quiz[counter].choices[1]);
+	$('#answer2').text(quiz[counter].choices[2]);
+	$('#answer3').text(quiz[counter].choices[3]);
+}
 
-				// when button is clicked reveal answer
 
-				$('.answer').on('click', function (e){
-					if (e = this.quiz)
+// client-sided validation
+function validate() {
+	if ($('input').is(':checked')) {
 
-				});
+		nextQuest(); // display next question
+	}
+	else {
+		alert("Please make a selection.");
+		counter--;
+	}
+}
 
-			};
+// display first question
+nextQuest();
 
-		},
 
-		// next: function () {
+// next button function
+$('#nextBtn').on('click', function() {
+	var answer = ($('input[name="choice"]:checked').val());
 
-		// 	$('.answer').on('click', function(e) {
-		// 		if (indexoFe == this.quiz[i].answer);
-		// 	});
-				
-		}	
-			for (var i = 0; i < 4; i++) {
-				var buttonAnswers = $('<button>');
-				buttonAnswers.attr('data-num', i);
-				buttonAnswers.addClass('nextQuestion');		
-			};
+	// increment score if answer is correct
+	if (answer == quiz[counter].correct) {
+		numCorrect++;
+	}
 
-		answer: function () {
+	counter++;
 
-			
-		
-		}
+
+	// display score screen
+	if (counter >= numQuestions) {
+		$('#main').hide().fadeIn("slow");
+		document.getElementById('main').innerHTML="Quiz Complete! You scored " + numCorrect + " out of " + numQuestions + "!";
+		return; // returns false *(there has to be a better way! figure it out.)*
+	}
+
+	validate();
+
+	// fade in new question
+	$('.card').hide().fadeIn("slow");
+	
+	// clear previous selection
+	$('input[name="choice"]').prop('checked', false);
+});
+
+
+// back button function
+$('#backBtn').on('click', function() {
+	if (quiz[counter] == 0) {
+		$('.card').hide().fadeIn("slow");
+
+	} else {
+		// fade out current question and fade in previous question
+		$('.card').hide().fadeIn("slow");
+		numCorrect--;
+		counter--;
 	}
 
 	
-
-triviaGame.start();
-stopwatch.run();
-
-
-
-
-
+	
+	// display previous question
+	nextQuest();	
+});
 
